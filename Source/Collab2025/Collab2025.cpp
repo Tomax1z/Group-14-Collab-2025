8f5a92d5f5b1666da68ebf2c36677167cdd65ab0
Tom Lawrence <114944480+Tomax1z@users.noreply.github.com>
2025-03-03 16:37:34 +0000

    Initial commit

---
+++ Source/Collab2025/Collab2025.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Collab2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Collab2025, "Collab2025" );
