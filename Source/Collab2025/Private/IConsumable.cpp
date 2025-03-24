cc58400ba3d4e5d1876547d8f08e588b7f38b52e
ash04 <33161248+Ashvin04@users.noreply.github.com>
2025-03-24 15:11:17 +0000

    wokring on energy

---
+++ Source/Collab2025/Private/IConsumable.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IConsumable.h"
+
+
+// Add default functionality here for any IIConsumable functions that are not pure virtual.
---
+++ Source/Collab2025/Private/IConsumable.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "IConsumable.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UIConsumable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class COLLAB2025_API IIConsumable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
